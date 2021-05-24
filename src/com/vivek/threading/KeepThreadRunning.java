70d47df8baa51eb8463b84ca3a86a34b2898641c
Vivek Jaiswal <vijaiswal@hotels.com>
2021-05-24 22:59:07 +0530

    Keep thread running alternately

--- src/com/vivek/threading/KeepThreadRunning.java
+++ src/com/vivek/threading/KeepThreadRunning.java
@@ -14,6 +14,21 @@ public class KeepThreadRunning {
         worker.stopThread();
     }
 
+    /**
+     * Here the process stops when the doWork() method returns false. It is also good style to allow your thread to be interrupted,
+     * especially if it needs to run for a long time.
+     * Note you cannot restart threads, once the run method has returned, the thread is reclaimed by the system.
+     * If you need more control over your threads you could create separate Worker and ThreadManager classes.
+     *
+     * To let the ThreadManager terminate the Worker, create a volatile boolean field in your Worker which is checked periodically:
+     * The Worker ends when interrupted or when the work is completed. Also the ThreadManager can request the Worker to stop by invoking the stopRunning() method.
+     *
+     * If your thread runs out of work it could also call the wait() method on the ThreadManager.
+     * This pauses the thread until it is notified that there is new work to do. The ThreadManager should call notify() or notifyAll() when new work arrives
+     *
+     * With this approach you can keep the Worker simple and only concerned with doing the work.
+     * The ThreadManager determines the number of threads and makes work available to them but does not need to know details of the actual work.
+     */
     private static class Worker extends Thread {
 
         private volatile boolean running;
